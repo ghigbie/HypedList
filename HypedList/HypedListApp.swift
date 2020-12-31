8eca7596388cfbcf5da595ddee5a4c198b7a4cf5
ghigbie <gehigbie@gmail.com>
2020-12-30 19:41:01 -0500

    Initial commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by George Higbie on 12/30/20.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
